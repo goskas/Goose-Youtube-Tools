841050ec0f194555d51924182a7fa30b7c7ff5bd
goska <goska.science@gmail.com>
2015-06-07 10:02:03 +0200

    Improved youtuubeAPIinterface module. Updated .gitignore

--- youtubeAPIinterface.py
+++ youtubeAPIinterface.py
@@ -40,16 +40,15 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__),
                          CLIENT_SECRETS_FILE))
 
-# This OAuth 2.0 access scope allows for read-only access to the authenticated
-# user's account, but not other types of account access.
+YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
 YOUTUBE_READONLY_SCOPE = "https://www.googleapis.com/auth/youtube.readonly"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def start_youtube():
+def start_youtube(scope):
     flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
         message=MISSING_CLIENT_SECRETS_MESSAGE,
-        scope=YOUTUBE_READONLY_SCOPE)
+        scope=scope)
 
     storage = Storage("%s-oauth2.json" % sys.argv[0])
     credentials = storage.get()
@@ -75,20 +74,26 @@ from  ura import *
 
 
 def get_video_duration(youtube, videoID, part='id, contentDetails'):
-    return get_video_details(youtube,
-                             videoID,
-                             part=part
-                            )['contentDetails']['duration']
+    return get_video_details(
+        youtube,
+        videoID,
+        part=part
+    )['contentDetails']['duration']
 
 def get_playlist_durations(youtube, playlistID):
     durations = []
-    all_videos = get_all_videos_in_playlist(youtube,
-                                            playlistID,
-                                            part='snippet,id')
+    all_videos = get_all_videos_in_playlist(
+        youtube,
+        playlistID,
+        part='snippet,id'
+    )
+    
     for video in all_videos:
         #print('video in get_playlist_duration', video)
-        duration = get_video_duration(youtube,
-                                         video['snippet']['resourceId']['videoId'])
+        duration = get_video_duration(
+            youtube,
+            video['snippet']['resourceId']['videoId']
+        )
         durations += [duration_to_seconds(duration)]
     return durations
 
@@ -97,8 +102,15 @@ def get_playlist_duration(youtube, playlistID):
     return len(d), sum(d)
 
 def get_video_details(youtube, videoID, part='id, snippet, contentDetails'):
-    video = youtube.videos().list(id=videoID,
-                                  part=part).execute()
+    video = youtube.videos().list(
+        id=videoID,
+        part=part
+    ).execute()
+    if len(video['items']) == 0:
+        print('Found no videos vi ID:', videoID)
+        print('Returning default')
+        videoID = 'ySU703VXSNY'
+        return get_video_details(youtube, videoID, part)
     return video['items'][0]
 
 def get_all_videos_in_playlist(youtube, playlistID, part='id, snippet'):
@@ -106,9 +118,11 @@ def get_all_videos_in_playlist(youtube, playlistID, part='id, snippet'):
     all_videos = []
     while True:
         ### ADD maxResults=50 v request
-        list_of_videos = youtube.playlistItems().list(part=part,
-                                                      playlistId=playlistID,
-                                                      pageToken=pageToken).execute()
+        list_of_videos = youtube.playlistItems().list(
+            part=part,
+            playlistId=playlistID,
+            pageToken=pageToken
+        ).execute()
 
         for video in list_of_videos['items']:
             all_videos += [video]
@@ -119,67 +133,136 @@ def get_all_videos_in_playlist(youtube, playlistID, part='id, snippet'):
     return all_videos
   
 
-def get_all_playlists_my_channel(youtube, what_to_print):
+def get_all_playlists_my_channel(youtube):
     """
     Finds all playlists on my channel excluding automatically created ones
     like: liked videos, watch later, favourites, history, uploaded
     This is all so far.
     """
     pageToken = ''
+    ret = dict()
     while True:
         ### ADD maxResults=50 v request
-        list_of_playlists = youtube.playlists().list(part='id, snippet',
-                                                     mine=True,
-                                                     pageToken=pageToken,
-                                                     maxResults=50).execute()
+        list_of_playlists = youtube.playlists().list(
+            part='id, snippet',
+            mine=True,
+            pageToken=pageToken,
+            maxResults=50).execute()
         for playlist in list_of_playlists['items']:
-            print(playlist['snippet']['title'], playlist['id'])
+            ret[playlist['snippet']['title']] = playlist['id']
         if 'nextPageToken' in list_of_playlists:
             pageToken = list_of_playlists['nextPageToken']
         else:
             break
+    ret2 = youtube.channels().list(
+        mine=True,
+        part="contentDetails"
+    ).execute()['items'][0]['contentDetails']['relatedPlaylists']
+    for key, item in ret2.items():
+        if key in ret:
+            print("key already in ret", key, item, ret[key])
+        ret[key] = item
+    return ret
 
-#get_all_playlists_my_channel(youtube, 'a')
-#for v in get_all_videos_in_playlist(youtube, 'FLp2jqHIbB9kavpnjrOo92eA'):
-#    print(v['snippet']['title'], v['id'])
+def get_ALL_playlists_my_channel(youtube):
+    ''' Obsolete -- use get_all_playlists_my_channel '''
+    playlists = youtube.channels().list(
+        mine=True,
+        part="contentDetails"
+    ).execute()['items'][0]['contentDetails']['relatedPlaylists']
+    print(playlists)
+
+def get_uploaded_playlist(youtube):
+    uploadedID = youtube.channels().list(
+        mine=True,
+        part="contentDetails"
+    ).execute()['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    #print(uploadedID)
+    return uploadedID
+
+import yaml
+if __name__ == '__main__':
+    """
+    Get all channels I am subscribed to.
+    """
+
+    youtube = start_youtube(YOUTUBE_READONLY_SCOPE)
+
+    subscriptions = youtube.subscriptions().list(
+        mine=True,
+        part='id,snippet',
+        maxResults = 50,
+        ).execute()['items']
+    for i, s in enumerate(subscriptions):
+        subscriptions[i]['snippet'].pop('description')
+        subscriptions[i]['snippet'].pop('thumbnails')
+        subscriptions[i]['snippet']['resourceId'].pop('kind')
+        subscriptions[i].pop('kind')
+        subscriptions[i]['snippet'].pop('channelId') # to je moj channelID
+    print()
+    print (yaml.dump(subscriptions))
+
+    """
+    WiP - s searchom se verjetno da OK narest, da dobiš seznam videov.
+    Edin ne vem kako dobro zna upoštevati razliko med published in created datomom.
+    In kaj je kateri in tako.
+    Pri ordering je - date = date created
+    Pol maš pa še published before/after parametra.
+    """
+
+if __name__ == '__main__' and False:
+    """
+    Trying to get titles of deleted videos.
+    """
+    youtube = start_youtube(YOUTUBE_READONLY_SCOPE)
+    playlistID = 'WLp2jqHIbB9kavpnjrOo92eA'
+    all_videos = get_all_videos_in_playlist(youtube, playlistID)
+    for i, video in enumerate(all_videos):
+        try: video['snippet'].pop('thumbnails')
+        except: print('Couldnt print', i)
+        print(i, video['snippet'])
+
+
+if __name__ == '__main__' and False:
+    youtube = start_youtube(YOUTUBE_READONLY_SCOPE)
+    uploaded = get_uploaded_playlist(youtube)
+    all_videos = get_all_videos_in_playlist(youtube, uploaded)
+    for video in all_videos:
+        a = get_video_details(youtube, video['snippet']['resourceId']['videoId'], 'snippet,status')
+        #print(video['snippet']['resourceId']['videoId'], a['snippet']["categoryId"], a['status']["privacyStatus"][:6], a['status']["publicStatsViewable"])
+        if int(a['snippet']["categoryId"]) not in [1, 17, 19, 20, 22, 23, 24, 27, 28]:
+            print (a['snippet']["categoryId"])
+            print(video['snippet']['resourceId']['videoId'], a['snippet']["categoryId"], a['status']["privacyStatus"][:6], a['status']["publicStatsViewable"])
+        continue
+        try:
+            print(video)
+            #print(video['snippet']['title'], '\t', video['snippet']['description'])
+        except:
+            print()
+            print('   '.join(video.keys()))
+            print('--------------------------------------------------------------------')
+            print(video['snippet']['resourceId'])
 
 if False:
     # Retrieve the contentDetails part of the channel resource for the
     # authenticated user's channel.
     channels_response = youtube.channels().list(
-      mine=True,
-      part="contentDetails"
+        mine=True,
+        part="contentDetails"
     ).execute()
-
-    print(channels_response["items"])
-
-            
-                
-    for channel in channels_response["items"]:
-        # From the API response, extract the playlist ID that identifies the list
-        # of videos uploaded to the authenticated user's channel.
-        print(channel)
-        print()
         
+    for channel in channels_response["items"]:
         uploads_list_id = channel["contentDetails"]["relatedPlaylists"]["watchLater"]
 
-        print ("Videos in list %s" % uploads_list_id)
-
-        # Retrieve the list of videos uploaded to the authenticated user's channel.
         playlistitems_list_request = youtube.playlistItems().list(
           playlistId=uploads_list_id,
           part="snippet",
           maxResults=50
         )
-
         while playlistitems_list_request:
           playlistitems_list_response = playlistitems_list_request.execute()
-
-          # Print information about each video.
           for playlist_item in playlistitems_list_response["items"]:
-            title = playlist_item["snippet"]["title"]
-            video_id = playlist_item["snippet"]["resourceId"]["videoId"]
-            print ("%s (%s)" % (title, video_id))
+            pass #do stuff
 
           playlistitems_list_request = youtube.playlistItems().list_next(
             playlistitems_list_request, playlistitems_list_response)
